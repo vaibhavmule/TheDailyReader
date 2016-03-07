f8b027ef5459cb16b8a8ad4492f229d6f9873a02
vaibhavmule <vaibhavmule135@gmail.com>
2016-03-07 18:17:58 +0530

    add creation of link

--- src/ReadingApp.js
+++ src/ReadingApp.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import _ from 'lodash'
+
 import ReadingList from './ReadingList';
 
 export default class ReadingApp extends Component {
@@ -7,25 +9,37 @@ export default class ReadingApp extends Component {
     super(props)
     this.state = {
       items: [],
-      text: ''
+      link: '',
+      title: '',
     }
-  }
+  };
 
-  onChange = (e) => {
-    this.setState({text: e.target.value});
+  onChangeLink = (e) => {
+    this.setState({link: e.target.value});
+  };
+
+  onChangeTitle = (e) => {
+    this.setState({title: e.target.value});
   };
 
   handleSubmit = (e) => {
     e.preventDefault();
-    var nextItems = this.state.items.concat([{text: this.state.text, id: Date.now()}]);
-    this.setState({items: nextItems, text: ''});
+
+    if (!_.includes(this.state.link, 'http')) {
+      this.state.link = 'http://' + this.state.link;
+    }
+    var nextItems = this.state.items.concat([{link: this.state.link, id: Date.now(), title: this.state.title}]);
+    this.setState({items: nextItems, link: '', title: ''});
   };
 
   render() {
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
-          <input onChange={this.onChange} value={this.state.text}/>
+          <label>link: </label>
+          <input onChange={this.onChangeLink} value={this.state.link}/>
+          <label>title: </label>
+          <input onChange={this.onChangeTitle} value={this.state.title}/>
           <button>Add</button>
         </form>
         <ReadingList items={this.state.items} />
--- src/ReadingList.js
+++ src/ReadingList.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 
 export default class ReadingList extends Component{
-	createItem = (item) => {
-      return <li key={item.id}>{item.text}</li>;
+  createItem = (item) => {
+  return (
+      <li key={item.id}>
+      	<a target="_blank" href={item.link}>{item.title}</a>
+      </li>
+    );
   };
 
   render() {  
-    return ( <ul>{this.props.items.map(this.createItem)}</ul> );
+    return (<ul>{this.props.items.map(this.createItem)}</ul>);
   }
 }
  
+	
