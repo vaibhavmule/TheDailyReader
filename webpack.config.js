95eb9a1da13042532b6b80a4d4c8a690538c34a0
Vaibhav Mule <vaibhavm@care24.co.in>
2016-09-20 23:40:23 +0530

    wip

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var path = require('path')
+var webpack = require('webpack')
+  
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  module: {
+    loaders: [{ 
+      test: /\.js$/, 
+      loaders: ['babel'],
+      exclude: /node_modules/,
+      include: __dirname
+    }]
+  }
+};
